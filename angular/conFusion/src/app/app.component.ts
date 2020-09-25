8886486f79a36aea91980bedfa902c6f095cd58e
Hector Marquez <himarquez@gmail.com>
2020-09-25 03:39:55 -0700

    250920200339

--- angular/conFusion/src/app/app.component.ts
+++ angular/conFusion/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'app-root', //etiqueta donde va a verse en el index <app-root>
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'conFusion con Angular';
+  title = 'Ristorante Con Fusion';
+  author = 'Hector Marquez'
 }
