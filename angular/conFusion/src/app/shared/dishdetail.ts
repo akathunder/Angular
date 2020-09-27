d7b3b5b1332e8adaf397796f8af0e0f0f16cc860
Hector Marquez <himarquez@gmail.com>
2020-09-26 21:37:17 -0700

    first week complete

---
+++ angular/conFusion/src/app/shared/dishdetail.ts
@@ -0,0 +1,16 @@
+export class DishDetail {
+    id: string;
+    name: string;
+    image: string;
+    category: string;
+    featured: boolean;
+    label: string;
+    price: string;
+    description: string;
+    comments: [{
+        rating: Number;
+        comment: string;
+        author: string;
+        date: string;
+    }]
+}
